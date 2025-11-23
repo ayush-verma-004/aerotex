ada44743c6b94a3f694cf866f9d957ac0aba8f45
ayush-verma-004 <ayushverma593773@gmail.com>
2025-11-23 15:00:06 +0530

    Add entities and enums

---
+++ Backend/monolithic/econe/src/main/java/com/javnic/econe/EconeApplication.java
@@ -0,0 +1,13 @@
+package com.javnic.econe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconeApplication.class, args);
+	}
+
+}
